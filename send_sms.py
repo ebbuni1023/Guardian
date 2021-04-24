9be95d49cb74f4d99d853a843c925a0d512fde60
junaama <0naama0@gmail.com>
2021-04-24 17:34:49 -0400

    update patient message in sms

--- send_sms.py
+++ send_sms.py
@@ -11,9 +11,12 @@ account_sid = os.environ['TWILIO_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
+# Patient message from keyboard.py output
+patient_msg = "Temporary Patient Message"
+
 message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     body=patient_msg,
                      from_='+14156514181',
                      to='+14845093855'
                  )
